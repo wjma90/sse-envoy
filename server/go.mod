0844dd8cd4f56efc6faf7aa575348b83730b974e
willian marchan <willianmarchan@Willians-MacBook-Pro.local>
2022-01-15 12:53:06 -0500

    initi

---
+++ server/go.mod
@@ -0,0 +1,8 @@
+module github.com/wjma90/simple-sse/server
+
+go 1.13
+
+require (
+	github.com/alexandrevicenzi/go-sse v0.0.0-20191008221740-c629c4a96646
+	github.com/google/uuid v1.1.1 // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/alexandrevicenzi/go-sse"
+)
+
+func main() {
+	port := ":" + os.Getenv("PORT")
+	eventType := os.Getenv("EVENT_TYPE")
+	dataEndpoint := os.Getenv("DATA_ENDPOINT")
+	certPath := os.Getenv("CERT_PATH")
+	keyPath := os.Getenv("KEY_PATH")
+
+	opt := &sse.Options{
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin":      "https://localhost:8080",
+			"Access-Control-Allow-Credentials": "true",
+			"Access-Control-Allow-Methods":     "GET, OPTIONS",
+			"Access-Control-Allow-Headers":     "Keep-Alive,X-Requested-With,Cache-Control,Content-Type,Last-Event-ID",
+		},
+	}
+	// Create the server.
+	s := sse.NewServer(opt)
+	defer s.Shutdown()
+
+	// Register with /events endpoint
+	http.Handle("/events", s)
+
+	// Dispatch messages
+	id := 1
+	go func() {
+		for {
+			joke := getDadJoke(dataEndpoint)
+			s.SendMessage("/events", sse.NewMessage(strconv.Itoa(id), joke, eventType))
+			id++
+			time.Sleep(10 * time.Second)
+		}
+	}()
+
+	http.ListenAndServeTLS(port, certPath, keyPath, nil)
+}
+
+func getDadJoke(url string) string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("Accept", "text/plain")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	joke := string(body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return joke
+}
